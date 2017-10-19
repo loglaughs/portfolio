d779baa2639724243b2588b98ae067054fc5c5a4
loglaughs <32936054+loglaughs@users.noreply.github.com>
2017-10-19 17:34:27 -0500

    Update clicker.js

--- clicker/clicker.js
+++ clicker/clicker.js
@@ -1,1 +1,19 @@
-
+document.getElementById("out").innerHTML = localStorage.clickcount;
+function count() {
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    }
+    else {
+      localStorage.clickcount = 1;
+    }
+  }
+  document.getElementById("out").innerHTML = localStorage.clickcount;
+  document.getElementById("reset").innerHTML = "";
+  document.getElementById("out").draggable = false;
+}
+function reset() {
+  localStorage.clickcount = 1;
+  document.getElementById("out").innerHTML = localStorage.clickcount;
+  document.getElementById("reset").innerHTML = "Count Reset!";
+}
