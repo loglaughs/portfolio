d16df77e6077efca7c13208d1f826430af5266cc
loglaughs <32936054+loglaughs@users.noreply.github.com>
2017-10-19 17:10:49 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+function home() {
+  window.location.href = "index.html";
+  return false;
+}
+function clicker() {
+  window.location.href = "/clicker/clicker.html";
+  return false;
+}
+function detector() {
+  window.location.href = "/detector/detector.html";
+  return false;
+}
+function calculator() {
+  window.location.href = "/calculator/calculator.html";
+  return false;
+}
+function about() {
+  window.location.href = "/about/about.html";
+  return false;
+}
