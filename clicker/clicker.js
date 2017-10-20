3ea6f6914401143305540c828137e95d466db052
loglaughs <32936054+loglaughs@users.noreply.github.com>
2017-10-20 17:45:42 -0500

    Update clicker.js

--- clicker/clicker.js
+++ clicker/clicker.js
@@ -17,3 +17,23 @@ function reset() {
   document.getElementById("out").innerHTML = localStorage.clickcount;
   document.getElementById("reset").innerHTML = "Count Reset!";
 }
+function home() {
+  window.location.href = "/portfolio/index.html";
+  return false;
+}
+function clicker() {
+  window.location.href = "portfolio/clicker/clicker.html";
+  return false;
+}
+function detector() {
+  window.location.href = "portfolio/detector/detector.html";
+  return false;
+}
+function calculator() {
+  window.location.href = "portfolio/calculator/calculator.html";
+  return false;
+}
+function about() {
+  window.location.href = "portfolio/about/about.html";
+  return false;
+}
