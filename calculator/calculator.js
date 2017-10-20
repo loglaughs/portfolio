42f2ecbb4eed91a14f0fd9227ad9b1e7120e4f6a
loglaughs <32936054+loglaughs@users.noreply.github.com>
2017-10-20 17:45:55 -0500

    Update calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,1 +1,20 @@
-
+function home() {
+  window.location.href = "/portfolio/index.html";
+  return false;
+}
+function clicker() {
+  window.location.href = "portfolio/clicker/clicker.html";
+  return false;
+}
+function detector() {
+  window.location.href = "portfolio/detector/detector.html";
+  return false;
+}
+function calculator() {
+  window.location.href = "portfolio/calculator/calculator.html";
+  return false;
+}
+function about() {
+  window.location.href = "portfolio/about/about.html";
+  return false;
+}
