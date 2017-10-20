068fef76cf965aeb0940cbc30b56e816145b85a9
loglaughs <32936054+loglaughs@users.noreply.github.com>
2017-10-20 17:45:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function home() {
-  window.location.href = "index.html";
+  window.location.href = "/portfolio/index.html";
   return false;
 }
 function clicker() {
-  window.location.href = "/clicker/clicker.html";
+  window.location.href = "portfolio/clicker/clicker.html";
   return false;
 }
 function detector() {
-  window.location.href = "/detector/detector.html";
+  window.location.href = "portfolio/detector/detector.html";
   return false;
 }
 function calculator() {
-  window.location.href = "/calculator/calculator.html";
+  window.location.href = "portfolio/calculator/calculator.html";
   return false;
 }
 function about() {
-  window.location.href = "/about/about.html";
+  window.location.href = "portfolio/about/about.html";
   return false;
 }
